6e9b30b2abdc3bec6dcd74a0dca150c184d99a54
Adrien Van Hyfte <adrien.vh@gmail.com>
2017-05-02 18:59:19 +0200

    Test

--- Gui/Web/js/main.js
+++ Gui/Web/js/main.js
@@ -2,6 +2,31 @@
 /*global $, WGo */
 
 $(function () {
-    var board = new WGo.Board(document.getElementById("board"), { width: 600 });
-    console.log(board);
+    var board = new WGo.Board(document.getElementById("board"), { width: 600 }),
+        tool = document.getElementById("tool"),
+        game = new WGo.Game(19, "ALL", false, false); // get the <select> element
+    
+    board.addEventListener("click", function (x, y) {
+        if (tool.value === "black") {
+            board.addObject({
+                x: x,
+                y: y,
+                c: WGo.B
+            });
+        } else if (tool.value === "white") {
+            board.addObject({
+                x: x,
+                y: y,
+                c: WGo.W
+            });
+        } else if (tool.value === "remove") {
+            board.removeObjectsAt(x, y);
+        } else {
+            board.addObject({
+                x: x,
+                y: y,
+                type: tool.value
+            });
+        }
+    });
 });
