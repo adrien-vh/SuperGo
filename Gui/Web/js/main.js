31d762f21676698eda90ace2a774d155dda40549
root <root@Salon.localdomain>
2017-05-02 08:04:23 +0200

    Ajout Web

---
+++ Gui/Web/js/main.js
@@ -0,0 +1,6 @@
+/*jslint browser: true, devel: true, node: true, sloppy: true, regexp: true */
+/*global $, WGo */
+
+$(function () {
+    var board = new WGo.Board(document.getElementById("board"), { width: 600 });
+});
